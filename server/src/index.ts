bba9cb4b6fed50d0ce34d150ab58b356cfe3b812
leslie-fi <leslie.turis1@gmail.com>
2021-03-20 15:52:58 -0400

    setup apollo server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,29 @@
 import { MikroORM } from '@mikro-orm/core';
-import { Post } from './entities/Post';
+// import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { HelloResolver } from './resolvers/hello';
 
 const main = async () => {
   const ORM = await MikroORM.init(mikroConfig);
-  await ORM.getMigrator().up();
-  // const post = ORM.em.create(Post, { title: `baby's first post` });
-  // await ORM.em.persistAndFlush(post);
+  // await ORM.getMigrator().up();
 
-  const posts = await ORM.em.find(Post, {});
-  console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log(`app is up and running on port ${process.env.PORT || 4000}`);
+  });
 };
 
 main().catch((err) => console.error(err));
